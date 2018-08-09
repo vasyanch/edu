35a430b3ba7e1a79d9422db7de35c098ba0c6a74
Vasily <vasyanch852@gmail.com>
2018-08-09 19:25:14 +0300

    'Lutz'

--- Python/Lutz_programming_python/chapter1/db_py/manager.py
+++ Python/Lutz_programming_python/chapter1/db_py/manager.py
@@ -1,7 +1,7 @@
 from person import Person
 
 class Manager(Person):
-    def __init__(self, name, age, pay=0):
+    def __init__(self, name, age, pay):
         Person.__init__(self, name, age, pay, 'manager')
     def giveRaise(self, percent, bonus=0.1):
         Person.giveRaise(self, percent + bonus)
